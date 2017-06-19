6d991b769b87591d9a8eaa8f3275e075f7e72ae3
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-06-19 11:00:07 +0700

    Init cache

--- scripts/init-cache.js
+++ scripts/init-cache.js
@@ -1,9 +1,8 @@
 const fs = require("fs")
-try {
-  let stateJson = fs.readdirSync(__dirname + "/../src/cache/state.json")
-} catch (err) {
-  console.log(err)
-  if (err.code == "ENOENT") {
-    fs.writeFileSync(__dirname + "/../src/cache/state.json", "{}")
-  }
+const path = require("path")
+
+const stateJsonFile = path.join(__dirname, "/../src/cache/state.json")
+
+if (!fs.existsSync(stateJsonFile)) {
+  fs.writeFileSync(stateJsonFile, "{}")
 }
